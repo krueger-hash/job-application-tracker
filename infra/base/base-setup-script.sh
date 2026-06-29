4b71b0c922c7d905d71f0002471544f976475fe2
kruegerhash <landolf.kr2@gmail.com>
2026-06-29 18:34:45 +0200

    chore: update initial setup scripts

--- infra/base/base-setup-script.sh
+++ infra/base/base-setup-script.sh
@@ -1,4 +1,8 @@
-# creates base setup for aws
+### Run from the root
+#### Creates the base setup for staging and production aws environments.
+
+# AWS BASE SETUP SETUP STAGING
+### creates base setup for aws
 sam deploy \
    -t infra/base/template.yaml \
    --stack-name job-application-tracker-base-staging \
@@ -8,14 +12,37 @@ sam deploy \
    --parameter-overrides Environment=staging \
    --profile staging
 
-# powershell command
+### powershell command
 : <<'COMMENT'
 sam deploy `
-   -t base/template.yaml `
+   -t infra/base/template.yaml `
    --stack-name job-application-tracker-base-staging `
    --region eu-central-1 `
    --resolve-s3 `
    --capabilities CAPABILITY_IAM `
    --parameter-overrides Environment=staging `
    --profile staging
+COMMENT
+
+# AWS BASE SETUP SETUP PRODUCTION
+### creates base setup for aws
+sam deploy \
+   -t infra/base/template.yaml \
+   --stack-name job-application-tracker-base-production \
+   --region eu-central-1 \
+   --resolve-s3 \
+   --capabilities CAPABILITY_IAM \
+   --parameter-overrides Environment=production \
+   --profile production
+
+### powershell command
+: <<'COMMENT'
+sam deploy `
+   -t infra/base/template.yaml `
+   --stack-name job-application-tracker-base-production `
+   --region eu-central-1 `
+   --resolve-s3 `
+   --capabilities CAPABILITY_IAM `
+   --parameter-overrides Environment=production `
+   --profile production
 COMMENT
